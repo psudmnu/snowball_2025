718246864ed16eecd6cf95b2c81a153a0c6083ad
Ethan Ritchey <84916194+EthanRithcey@users.noreply.github.com>
2021-06-14 11:06:46 -0400

    Update DMXDetectorConstruction.hh
    I removed the photomultiplier by deleting pmt_log and pmt_phys. I also cleaned up the code a little by commenting out vesseltop_phys1 and vesselbottom_phys2, I only commented these out because they are associated with lagging properties

--- underground_physics/include/DMXDetectorConstruction.hh
+++ underground_physics/include/DMXDetectorConstruction.hh
@@ -110,10 +110,9 @@ private:
 
   
 
-  
+  /*
   G4VPhysicalVolume* vesseltop_phys1;
   G4VPhysicalVolume* vesselbottom_phys2;
-  /*
   G4LogicalVolume*   GXe_log;
   G4VPhysicalVolume* GXe_phys;  
   G4LogicalVolume*   gaslag_log;
@@ -125,8 +124,6 @@ private:
   G4LogicalVolume*   liqLag_log; 
   G4VPhysicalVolume* liqLag_phys;  
   */
-  G4LogicalVolume*   pmt_log;   
-  G4VPhysicalVolume* pmt_phys; 
   G4LogicalVolume*   phcath_log;
   
 
